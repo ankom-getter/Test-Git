0274f09a183ada916b981d4d9ac263fd30a8b693
Ruslan Voinarovskyi <economy.savvier_0q@icloud.com>
2023-03-05 20:48:16 +0100

    app

--- Test Git/ContentView.swift
+++ Test Git/ContentView.swift
@@ -23,6 +23,8 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
         
+        //test branch 1
+        
         //test app
     }
 }

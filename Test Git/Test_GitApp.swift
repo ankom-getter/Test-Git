a9d4a5dcbc037a0bb4f86754469aa9b86b727162
Ruslan Voinarovskyi <economy.savvier_0q@icloud.com>
2023-03-05 20:41:54 +0100

    Initial Commit

---
+++ Test Git/Test_GitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_GitApp.swift
+//  Test Git
+//
+//  Created by Ruslan Voinarovskyi on 05.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct Test_GitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
